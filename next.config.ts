9bd9451a9abc30570dc01e40b29c1ea8f208e795
Gildácio Lopes <gildacioj399@gmail.com>
2025-08-07 17:27:13 -0300

    feat: add product list and product item components

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fsc-projects-static.s3.us-east-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "d4lgxe9bm8juw.cloudfront.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
