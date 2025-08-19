1720867c8354ca47655eef693cdf88d826167451
Gildácio Lopes <gildacioj399@gmail.com>
2025-08-19 13:55:53 -0300

    feat: enhance add-to-cart button to require user login before adding products to cart

--- src/app/product-variant/[slug]/components/add-to-cart-button.tsx
+++ src/app/product-variant/[slug]/components/add-to-cart-button.tsx
@@ -2,9 +2,11 @@
 
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
+import { toast } from "sonner";
 
 import { addProductToCart } from "@/actions/add-cart-product";
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 
 interface AddToCartButtonProps {
   productVariantId: string;
@@ -16,6 +18,7 @@ const AddToCartButton = ({
   quantity,
 }: AddToCartButtonProps) => {
   const queryClient = useQueryClient();
+  const { data: session } = authClient.useSession();
   const { mutate, isPending } = useMutation({
     mutationKey: ["addProductToCart", productVariantId, quantity],
     mutationFn: () =>
@@ -33,7 +36,13 @@ const AddToCartButton = ({
       size="lg"
       variant="outline"
       disabled={isPending}
-      onClick={() => mutate()}
+      onClick={() => {
+        if (!session?.user) {
+          toast.info("Você precisa estar logado para realizar a compra.");
+          return;
+        }
+        mutate();
+      }}
     >
       {isPending && <Loader2 className="animate-spin" />}
       Adicionar à sacola
